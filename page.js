579725efa1a4cf78e3648e39bb31f8c0561af079
haku <altanpurev@wisc.edu>
2025-02-27 04:58:08 -0500

    add some comments, and some formatting

--- page.js
+++ page.js
@@ -1,7 +1,13 @@
+// (uses jquery)
+// wait for the page to load first before we start doing stuff
 $(document).ready(function () {
+    //instead of having to copy and paste the navbar code into every page, we can just load it in
+    //from a separate file
     $(".header").load("navbar.html", function () {
+        //after it loads, fetch the current url we're at
         let currentPath = window.location.pathname.split("/").pop();
 
+        //add the active css class to highlight the current page in the navbar
         $(".nav a").each(function () {
             let linkPath = $(this).attr("href");
             if (linkPath === currentPath) {
@@ -10,6 +16,7 @@ $(document).ready(function () {
         });
     });
 
+    //for mobile/tablet users, handle the burger menu state
     $(document).click(function (event) {
         var target = $(event.target);
         if (!target.closest('.navbar-container').length) {
