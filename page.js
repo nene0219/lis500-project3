c5c15d5aa6a9a43100614a7372515bb221e53483
haku <altanpurev@wisc.edu>
2025-02-27 03:56:47 -0500

    styling changes, navbar changes, content styling changes

--- page.js
+++ page.js
@@ -1,3 +1,23 @@
-$(document).ready(function(){
-    $(".header").load("navbar.html");
+$(document).ready(function () {
+    $(".header").load("navbar.html", function () {
+        let currentPath = window.location.pathname.split("/").pop();
+
+        $(".nav a").each(function () {
+            let linkPath = $(this).attr("href");
+            if (linkPath === currentPath) {
+                $(this).addClass("active");
+            }
+        });
+    });
+
+    $(document).click(function (event) {
+        var target = $(event.target);
+        if (!target.closest('.navbar-container').length) {
+            $('#nav-toggle').prop('checked', false);
+        }
+    });
+
+    $('.nav a').click(function () {
+        $('#nav-toggle').prop('checked', false);
+    });
 });
